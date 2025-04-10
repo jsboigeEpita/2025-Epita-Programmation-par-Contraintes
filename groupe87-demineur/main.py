d89bd2027bdb404e297899746c49f1a7efa619a3
RayanDri <rayan.drissi@icloud.com>
2025-04-11 00:44:51 +0200

    FEAT: format code for improved readability and fix grid6 definition in main.py

--- groupe87-demineur/main.py
+++ groupe87-demineur/main.py
@@ -1,5 +1,6 @@
 from constraint import Problem, ExactSumConstraint
 
+
 class MinesweeperGrid:
     def __init__(self, grid, size):
         self.grid = grid
@@ -18,6 +19,7 @@ class MinesweeperGrid:
         return {pos: (solution[pos] if val == "?" else val)
                 for pos, val in self.grid.items()}
 
+
 class MinesweeperSolver:
     def __init__(self, minesweeper_grid):
         self.grid_obj = minesweeper_grid
@@ -30,13 +32,16 @@ class MinesweeperSolver:
             self.problem.addVariable(pos, [0, 1])
         for pos, val in grid.items():
             if val != "?":
-                neighbors = [n for n in self.grid_obj.get_neighbors(pos) if grid[n] == "?"]
+                neighbors = [n for n in self.grid_obj.get_neighbors(
+                    pos) if grid[n] == "?"]
                 if neighbors:
-                    self.problem.addConstraint(ExactSumConstraint(val), neighbors)
+                    self.problem.addConstraint(
+                        ExactSumConstraint(val), neighbors)
 
     def solve(self):
         self.setup_problem()
         return self.problem.getSolutions()
+
 
 class PrettyPrinter:
     @staticmethod
@@ -49,6 +54,7 @@ class PrettyPrinter:
                 row += f"| {grid[(i, j)]} "
             print(row + "|")
             print(border)
+
 
 if __name__ == "__main__":
     grid1 = {
@@ -79,17 +85,17 @@ if __name__ == "__main__":
         (1, 0): "?", (1, 1): "?", (1, 2): "?",
         (2, 0): 0, (2, 1): "?", (2, 2): 0,
     }
-    
+
     grid6 = {
-    (0,0): 1, (0,1): "?", (0,2): "?", (0,3): 1, (0,4): 0, (0,5): 0,
-    (1,0): "?", (1,1): "?", (1,2): "?", (1,3): "?", (1,4): 0, (1,5): 0,
-    (2,0): "?", (2,1): 2, (2,2): 2, (2,3): "?", (2,4): 1, (2,5): 0,
-    (3,0): 1, (3,1): "?", (3,2): "?", (3,3): "?", (3,4): 1, (3,5): 0,
-    (4,0): 0, (4,1): 1, (4,2): 2, (4,3): 2, (4,4): 1, (4,5): 0,
-    (5,0): 0, (5,1): 0, (5,2): 1, (5,3): "?", (5,4): 1, (5,5): 0,
-}
-    
-    grids = [(grid1, 5), (grid2, 4), (grid3, 3), (grid4, 3), (grid5, 3)]
+        (0, 0): 1, (0, 1): "?", (0, 2): "?", (0, 3): 1, (0, 4): 0, (0, 5): 0,
+        (1, 0): "?", (1, 1): "?", (1, 2): "?", (1, 3): "?", (1, 4): 0, (1, 5): 0,
+        (2, 0): "?", (2, 1): 2, (2, 2): 2, (2, 3): "?", (2, 4): 1, (2, 5): 0,
+        (3, 0): 1, (3, 1): "?", (3, 2): "?", (3, 3): "?", (3, 4): 1, (3, 5): 0,
+        (4, 0): 0, (4, 1): 1, (4, 2): 2, (4, 3): 2, (4, 4): 1, (4, 5): 0,
+        (5, 0): 0, (5, 1): 0, (5, 2): 1, (5, 3): "?", (5, 4): 1, (5, 5): 0,
+    }
+
+    grids = [(grid1, 5), (grid2, 4), (grid3, 3), (grid4, 3), (grid5, 3), (grid6, 6)]
 
     for idx, (grid, size) in enumerate(grids, start=1):
         print(f"Grille {idx}")
