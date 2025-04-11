f11616a8b45f92936cda3f34f4c0c5ee874189e8
andy.shan <andy.shan@epita.fr>
2025-04-11 04:21:07 +0200

    feat: chatGPT function calling

--- subject23/UnityProject/MultiBotWarehouse/Assets/Scripts/LLM Interactions/ApiInteraction.cs
+++ subject23/UnityProject/MultiBotWarehouse/Assets/Scripts/LLM Interactions/ApiInteraction.cs
@@ -13,15 +13,78 @@ public class ApiInteraction : MonoBehaviour
     [SerializeField]
     private ApiKeys apiKeys;
 
-    private static Dictionary<string, Func<Dictionary<string, string>, Task<string>>> functionMapping = new Dictionary<string, Func<Dictionary<string, string>, Task<string>>>
-    {
+    Task<string> currentLoop = null;
+    [SerializeField]
+    private bool inputButton = false;
+    [SerializeField]
+    private string inputText;
+    private string systemMessage = "You are a helpful assistant. We provide you the list of functions that you can call\n" +
+        "You can call them by using the function name and providing the required arguments in JSON format." +
+        "Your goal is to help the user by calling the functions and providing the results.\n" + 
+        "You need to evaluate the user input and decide whether to call a function or not and which one to call.\n" +
+        "You can call the functions by using the function name and providing the required arguments.\n" +
+        "It is your job to decide the parameters to pass to the function.\n" +
+        "If you don't want to call any function, just respond with \"END\".\n" +
+        "I zqnt you to output the function call in JSON format.\n" +
+        "The function call should be in the following format:\n" +
+        '{' + "\n" +
+        "  \"name\": \"function_name\",\n" +
+        "  \"arguments\": {\n" +
+        "    \"arg1\": \"value1\",\n" +
+        "    \"arg2\": \"value2\"\n" +
+        "  }\n" +
+        "}\n";
 
-    };
+    private void Update()
+    {
+        if (inputButton)
+        {
+            if (currentLoop == null)
+                currentLoop = StartConversationAsync(systemMessage, inputText, "gpt-4o-mini", true, true);
+
+            if (currentLoop.Status == TaskStatus.RanToCompletion)
+            {
+                Debug.Log("Received the following message: " + currentLoop.Result);
+                currentLoop = null;
+                inputButton = false;
+            }
+        }
+    }
+    public static Task<string> function1(int a, string b)
+    {
+        return Task.Run(() =>
+        {
+            // Simulate some processing
+            System.Threading.Thread.Sleep(1000);
+            return $"Function 1 called with a: {a}, b: {b}";
+        });
+    }
+
+    public static Task<string> function1Dico(Dictionary<string, string> args)
+    {
+        int a = int.Parse(args["a"]);
+        string b = args["b"];
+        return function1(a, b);
+    }
+
+    private static Dictionary<string, Func<Dictionary<string, string>, Task<string>>> functionMapping = new Dictionary<string, Func<Dictionary<string, string>, Task<string>>>    
+    {};
 
     private static List<FunctionDefinition> functionDefinitions = new List<FunctionDefinition>
-    {
+    ();
 
-    };
+    private void Awake()
+    {
+        functionMapping.Add("function1", function1Dico);
+
+        var properties = new { a = new ArgProperties() { type = "integer", description = "the desc of the first arg" },
+            b = new ArgProperties() { type = "string", description = "the desc of the second arg" } };
+
+    
+        functionDefinitions.Add(new FunctionDefinition("function1", "This is the first function",
+            new FunctionParameters("object", properties, new string[] {"a", "b"}, false)
+        ));
+    }
 
     #region JSON Objects
     [System.Serializable]
@@ -35,6 +98,51 @@ public class ApiInteraction : MonoBehaviour
     }
 
     [System.Serializable]
+    public class FunctionParameters
+    {
+        [JsonProperty("type")]
+        public string type { get; set; }
+
+        [JsonProperty("properties")]
+        public object properties { get; set; }
+
+        [JsonProperty("required")]
+        public string [] required { get; set; }
+
+        [JsonProperty("additionalProperties")]
+        public bool additionalProperties { get; set; }
+
+        public FunctionParameters(string type, object properties, string[] required, bool additionalProperties)
+        {
+            this.type = type;
+            this.properties = properties;
+            this.required = required;
+            this.additionalProperties = additionalProperties;
+        }
+    }
+
+    [System.Serializable]
+    public class ArgProperties
+    {
+        [JsonProperty("type")]
+        public string type { get; set; }
+
+        [JsonProperty("description")]
+        public string description { get; set; }
+    }
+
+    [System.Serializable]
+    public class Parameters
+    {
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("arguments")]
+        public string arguments { get; set; }
+    }
+
+
+    [System.Serializable]
     public class FunctionDefinition
     {
         [JsonProperty("name")]
@@ -44,7 +152,14 @@ public class ApiInteraction : MonoBehaviour
         public string description { get; set; }
 
         [JsonProperty("parameters")]
-        public string parameter { get; set; }
+        public FunctionParameters parameter { get; set; }
+
+        public FunctionDefinition(string name, string description, FunctionParameters parameter)
+        {
+            this.name = name;
+            this.description = description;
+            this.parameter = parameter;
+        }
     }
 
     [System.Serializable]
